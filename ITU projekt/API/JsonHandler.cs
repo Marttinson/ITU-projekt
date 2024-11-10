fbb8f5a98039b01cca8981923e3a4bf567081d82
Martin <marybnikar1@gmail.com>
2024-11-11 00:51:38 +0100

    Přidání souborů pro práci s JSON soubory pro pozdější použití

---
+++ ITU projekt/API/JsonHandler.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+public class Question
+{
+    public int ID { get; set; }
+
+    public string Type { get; set; }
+    public string QuestionText { get; set; }
+    public string Answer { get; set; }
+}
+
+public class JsonHandler
+{
+    private string _filePath;
+
+    public JsonHandler(string filePath)
+    {
+        _filePath = filePath;
+    }
+
+    public List<Question> LoadQuestions()
+    {
+        try
+        {
+            // Načítání JSON obsahu ze souboru
+            string jsonContent = File.ReadAllText(_filePath);
+
+            //Console.WriteLine(jsonContent);  // Výpis načteného obsahu pro kontrolu
+
+            // Načítání a deserializace JSON pole
+            using (JsonDocument doc = JsonDocument.Parse(jsonContent))
+            {
+                JsonElement root = doc.RootElement;
+                if (root.TryGetProperty("questions", out JsonElement questionsElement))
+                {
+                    List<Question> questions = new List<Question>();
+
+                    foreach (JsonElement questionElement in questionsElement.EnumerateArray())
+                    {
+                        Question question = new Question
+                        {
+                            ID = questionElement.GetProperty("ID").GetInt32(),
+                            Type = questionElement.GetProperty("Type").GetString(),
+                            QuestionText = questionElement.GetProperty("QuestionText").GetString(),
+                            Answer = questionElement.GetProperty("Answer").GetString()
+                        };
+                        questions.Add(question);
+                    }
+
+                    return questions;
+                }
+                else
+                {
+                    Console.WriteLine("JSON soubor neobsahuje žádné otázky (JsonHandler.cs)");
+                    return new List<Question>();
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+            return new List<Question>();
+        }
+    }
+}
---
+++ ITU projekt/API/QuestionUtils.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class QuestionUtils
+{
+    private List<Question> _questions;
+
+    // Konstruktor pro předání seznamu otázek
+    public QuestionUtils(List<Question> questions)
+    {
+        _questions = questions;
+    }
+
+    // Metoda pro vyhledání otázky podle ID
+    public Question FindQuestionById(int id)
+    {
+        foreach (var question in _questions)
+        {
+            if (question.ID == id)
+                return question;
+        }
+        return null;
+    }
+
+    // Metoda pro výběr X náhodných otázek
+    public List<Question> GetRandomQuestions(int count)
+    {
+        Random random = new Random();
+        return _questions.OrderBy(x => random.Next()).Take(count).ToList();
+    }
+
+    // Metoda pro přidání nové otázky
+    public void AddQuestion(Question question)
+    {
+        _questions.Add(question);
+    }
+
+    // Metoda pro odstranění otázky podle ID
+    public bool RemoveQuestionById(int id)
+    {
+        var question = FindQuestionById(id);
+        if (question != null)
+        {
+            _questions.Remove(question);
+            return true;
+        }
+        return false;
+    }
+}
